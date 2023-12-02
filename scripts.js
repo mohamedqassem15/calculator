2e4945a559bdfa7bf8e9117965be79b309dc4252
Mohamed <102714904+mohamedqassem15@users.noreply.github.com>
2023-12-01 22:48:34 -0500

    edited calcurotr

--- scripts.js
+++ scripts.js
@@ -10,19 +10,97 @@ let operator;
 function operate (num1, num2, operation){
     switch (operation) {
         case "+":
-            add(num1, num2);
+            return add(num1, num2);
             break;
         case "-":
-            subtract(num1, num2);
+            return subtract(num1, num2);
             break;
         case "*":
-            multiply(num1, num2);
+            return multiply(num1, num2);
             break;
         case "/":
-            divide(num1,num2);
+            return divide(num1,num2);
             break;
         default:
             console.log("Not Valid");
     }
 
 };
+
+operatorsTextContent = ["+", "-", "*", "/", "=", "clear"];
+
+const container = document.getElementById('container');
+const display = document.getElementById('display');
+
+let displayText = "";
+display.innerText = displayText;
+
+let userInput = "";
+let answer;
+for (let i = 0; i < 16; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        square.style.width = `${320 / 4}px`;
+        square.style.height = `${320 / 4}px`;
+        i <= 9 ? square.innerText = i :  square.innerText = operatorsTextContent[i - 10];
+        container.appendChild(square);
+
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('hovered');
+            square.style.backgroundColor = "gray";
+
+        });
+
+        square.addEventListener('mouseleave', () => {
+            square.classList.remove('hovered');
+            square.style.backgroundColor = '';
+        });
+
+
+    
+}
+
+let clearInput = false;
+
+container.addEventListener('click', (event) => {
+    userInput += event.target.innerText;
+    if(clearInput){
+        display.innerText = "" ;
+    }
+    clearInput = false;
+    
+
+    if (event.target.innerText === "clear") {
+        display.innerText = "";
+    } else if (event.target.innerText === "=") {
+        keepGoing = false;
+        do {
+
+            let operatorIndex = userInput.search(/[\+\-\*\/]/);
+
+
+            let num1 = parseInt(userInput.substring(0, operatorIndex), 10);
+            let num2 = parseInt(userInput.substring(operatorIndex + 1, userInput.length - 1), 10);
+            operator = userInput[operatorIndex];
+    
+            answer = operate(num1, num2, operator);
+            console.log(answer);
+            display.innerText = answer;
+            clearInput = true;
+
+            console.log(event.target.innerText);
+
+        }while(keepGoing);
+
+
+        
+
+    } else {
+        display.innerText += event.target.innerText;
+    }
+
+});
+
+
+
+
